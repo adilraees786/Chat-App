35055f5d3488e60f9ea602ae9619a746071cf330
adilraees786 <adilraees911@gmail.com>
2024-09-09 15:52:10 +0500

    chat

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./screens/Login";
 import Signup from "./screens/Signup";
 // import Loading from "./screens/Loading";
 import Home from "./screens/Home";
+import Chat from "./screens/Chat"
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
     // { path: "/loading", element: <Loading/>, },
     { path: "*", element: <NotFound/>, },
     { path: "/home", element: <Home/>, },
+    { path: "/chat", element: <Chat/>, },
     { path: "/login", element: <Login/>, },
     { path: "/signup", element: <Signup/>, },
   ]);
